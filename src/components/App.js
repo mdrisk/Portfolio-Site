77e831771c58d3afd9738d42b9d6d4279fac2eb4
mdrisk <mrisk@westclarkschools.com>
2018-02-28 14:19:14 -0500

    Commit5 pt2 - Small additions

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import logo from "../logo.svg";
-import "../style/App.css";
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../style/App.css';
 
-import NavTest from "./navbar";
-import ImgCarousel from "./img_carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Header from "./header.js";
-import PictureCombo from "./picture_combo";
-import FlipCard from "./flip_cards";
+import NavTest from './navbar';
+import ImgCarousel from './img_carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Header from './header.js';
+import PictureCombo from './picture_combo';
+import FlipCard from './flip_cards';
 
 class App extends Component {
   render() {
@@ -16,10 +16,16 @@ class App extends Component {
         <NavTest />
         <Header />
         <div>
+          <div className="d-flex justify-content-center">
+            <h2 className="App-intro ">
+              Hello and Welcome! Pull up a chair, a seat, a stool and gather
+              &#39;round as I weave my tale. Stories of adventure! From living
+              and teaching in Japan, to living and teaching here in the states.
+              And now I set forth on my greatest adventure yet, becoming a
+              professional developer!
+            </h2>
+          </div>
           <FlipCard />
-          <p className="App-intro">
-            Don&#39;t wanna have your hand where your hip be at!
-          </p>
           <PictureCombo />
           <ImgCarousel />
         </div>
