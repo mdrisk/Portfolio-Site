2448711b8777ea49851591329b678e697ec35e1b
mdrisk <mrisk@westclarkschools.com>
2018-03-01 05:15:24 -0500

    Commit 8 - more corrections

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,9 @@ export default () => {
     <header className="App-header">
       <div className="App-title">
         <div>
-          <h1>Matthew Risk&#39;s</h1>
+          <h1>Matt Risk&#39;s</h1>
           <h2>Winding Journey</h2>
-          <h2>A Developer&#39;s Tale</h2>
+          <h2>-A Developer&#39;s Tale-</h2>
         </div>
       </div>
     </header>
